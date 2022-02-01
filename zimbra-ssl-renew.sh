1b097cdca5f4cf0b9a62922bc15dad6da872fdda
AlexandruMindra <id.meu40@yahoo.com>
2022-02-01 11:40:48 +0200

    Update zimbra-ssl-renew.sh

--- zimbra-ssl-renew.sh
+++ zimbra-ssl-renew.sh
@@ -7,19 +7,24 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-read -p 'letsencrypt_email [xx@xx.xx]: ' letsencrypt_email
 read -p 'mail_server_url [xx.xx.xx]: ' mail_server_url
 
 # Check All variable have a value
-if [ -z $mail_server_url ] || [ -z $letsencrypt_email ]
+if [ -z $mail_server_url ]
 then
    echo run script again please insert all value. do not miss any value
 else
 
+rm /opt/zimbra/ssl/zimbra/commercial/commercial.key
+
+rm /tmp/ISRG-X1.pem
+
+rm /opt/zimbra/ssl/letsencrypt/*
+
 su - zimbra -c 'zmcontrol stop'
 
 /etc/scripts/stop
-certbot  certonly --standalone --non-interactive --agree-tos --email $letsencrypt_email -d $mail_server_url --hsts --preferred-chain "ISRG Root X1"
+certbot  renew --standalone --non-interactive --dry-run --hsts --preferred-chain "ISRG Root X1"
 /etc/scripts/firewall
 
 cp /etc/letsencrypt/live/$mail_server_url/privkey.pem /opt/zimbra/ssl/zimbra/commercial/commercial.key
@@ -27,7 +32,7 @@ chown zimbra:zimbra /opt/zimbra/ssl/zimbra/commercial/commercial.key
 wget -O /tmp/ISRG-X1.pem https://letsencrypt.org/certs/isrgrootx1.pem.txt
 cat /tmp/ISRG-X1.pem >> /etc/letsencrypt/live/$mail_server_url/chain.pem
 
-mkdir /opt/zimbra/ssl/letsencrypt
+#mkdir /opt/zimbra/ssl/letsencrypt
 cp /etc/letsencrypt/live/$mail_server_url/*.pem /opt/zimbra/ssl/letsencrypt/
 chown zimbra:zimbra /opt/zimbra/ssl/letsencrypt/
 chown zimbra:zimbra /opt/zimbra/ssl/letsencrypt/* -R
